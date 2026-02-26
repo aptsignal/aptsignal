867910153a3a6f474134cb320793a1d9b4561d80
aptsignal <maybetop1004@gmail.com>
2026-02-26 20:21:36 +0900

    Add index page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>AptSignal</h1>
+      <p>서울 아파트 투자 의사결정을 돕는 데이터 엔진</p>
+    </main>
+  );
+}
